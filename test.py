93f52b76fd80d77186db3c0a6229c403160fba45
cod3breaker <ravisherath@gmail.com>
2020-01-26 20:06:09 +1100

    adding bootstrap styles with code snippets

--- test.py
+++ test.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Back to basics - Python",
+        "content": "Back to basics serious for Python 3",
+        "date_posted": "26/01/2020"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Head First - Python",
+        "content": "Lessons from Head First - Python book",
+        "date_posted": "27/01/2020"
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
