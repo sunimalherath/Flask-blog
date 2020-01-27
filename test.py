c36e25ec5265f075db27db406de5a577a253564a
cod3breaker <ravisherath@gmail.com>
2020-01-27 18:02:16 +1100

    adding flask flash alert for registering user

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistraionForm, LoginForm
 
 app = Flask(__name__)
@@ -33,9 +33,12 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistraionForm()
+    if form.validate_on_submit():
+        flash(f'Account created successfully for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
