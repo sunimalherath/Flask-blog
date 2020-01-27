d47a494a53177b7868dc2bd744ddbdeb446ae485
cod3breaker <ravisherath@gmail.com>
2020-01-27 15:41:14 +1100

    adding login and register forms

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistraionForm, LoginForm
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'd2ce6f77363d58f974669fd1db2b46c5'
 
 posts = [
     {
@@ -27,6 +31,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistraionForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
